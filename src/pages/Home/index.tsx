757dee89b94f08eaed61bf643a988c7f5f13a32f
Yago Juan <yago.juan@teclat.com.br>
2021-05-17 13:27:51 -0300

    Ajustando a responsividade na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { MainContainer } from "./styles";
 import {
   Description,
   DescriptionContainer,
@@ -14,25 +15,27 @@ export default function Home() {
     <HomePage>
       <Header />
       <main>
-        <LogoContainer>
-          <img src="assets/logo.png" alt="Logo Radinâmico" />
-        </LogoContainer>
-        <DescriptionContainer>
-          <Title>Radinâmico</Title>
-          <Separator />
-          <Description>
-            <p>
-              Esse projeto foi criado para ajudar VOCÊ que é aluno(a) ou
-              profissional da área da saúde em geral, com um FOCO TOTAL EM
-              RADIOLOGIA.
-            </p>
-            <p>
-              Como o próprio nome do projeto já diz, adquirindo conhecimentos de
-              uma forma mais dinâmica. 😉
-            </p>
-            <p>Sou o Prof. Matheus Albuquerque e espero que gostem!!</p>
-          </Description>
-        </DescriptionContainer>
+        <MainContainer>
+          <LogoContainer>
+            <img src="assets/logo.png" alt="Logo Radinâmico" />
+          </LogoContainer>
+          <DescriptionContainer>
+            <Title>Radinâmico</Title>
+            <Separator />
+            <Description>
+              <p>
+                Esse projeto foi criado para ajudar VOCÊ que é aluno(a) ou
+                profissional da área da saúde em geral, com um FOCO TOTAL EM
+                RADIOLOGIA.
+              </p>
+              <p>
+                Como o próprio nome do projeto já diz, adquirindo conhecimentos
+                de uma forma mais dinâmica. 😉
+              </p>
+              <p>Sou o Prof. Matheus Albuquerque e espero que gostem!!</p>
+            </Description>
+          </DescriptionContainer>
+        </MainContainer>
       </main>
       <Footer />
     </HomePage>
