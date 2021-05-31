13e57187f984bc491179b3dc0c31876064550d6d
Yago Juan <yago.juan@teclat.com.br>
2021-05-31 12:21:46 -0300

    Iniciando a utilizaçao do map

--- src/pages/AnatomyGroup/index.tsx
+++ src/pages/AnatomyGroup/index.tsx
@@ -1,18 +1,41 @@
-import { useParams } from "react-router";
+// import { useParams } from "react-router";
+import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import { AnatomyGroupContainer } from "./styles";
+import { AnatomyGroupContainer, ImageContainer } from "./styles";
 
-interface ParamsProps {
-  slug: string;
-}
+// interface ParamsProps {
+//   slug: string;
+// }
 
 export default function AnatomyGroup() {
-  const { slug } = useParams<ParamsProps>();
+  // const { slug } = useParams<ParamsProps>();
+
+  function showMessage() {
+    alert("ok");
+  }
 
   return (
     <AnatomyGroupContainer>
-      <Header /> <div>Grupo anatomico: {slug}</div> <Footer />
+      <Header />
+      <main>
+        <ImageContainer>
+          <img
+            src="/assets/HeadPoints/cranio-frontal.png"
+            alt="Cranio frontal"
+            useMap="#frontal"
+          />
+          <map name="frontal">
+            <area
+              shape="rect"
+              coords="228,114,258,155"
+              onMouseOver={showMessage}
+              alt="Ponto 1"
+            />
+          </map>
+        </ImageContainer>
+      </main>
+      <Footer />
     </AnatomyGroupContainer>
   );
 }
--- src/pages/AnatomyGroup/styles.ts
+++ src/pages/AnatomyGroup/styles.ts
@@ -1,5 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import theme from "../../styles/theme";
 
 export const AnatomyGroupContainer = styled.div`
   width: 100%;
+`;
+export const ImageContainer = styled.div`
+  width: 100%;
+  min-width: 400px;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.colors.black};
+  padding: 50px auto;
+  & > img {
+    max-width: 90%;
+    height: 100%;
+    max-height: 700px;
+  }
 `;
